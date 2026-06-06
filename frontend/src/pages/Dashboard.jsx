50792e83e1cc9e736a5ca1345e572f6876eab5b3
vishanthkannan <vishanthkannan777@gmail.com>
2026-06-06 23:13:30 +0530

    Increase loader size and set it to green color, fix video opacity typo

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -572,7 +572,7 @@ const Dashboard = () => {
             transition={{ duration: 0.3 }}
           >
             <div className="loading">
-              <svg height="48px" width="64px">
+              <svg height="96px" width="128px" viewBox="0 0 64 48">
                 <polyline id="back" points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24"></polyline>
                 <polyline id="front" points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24"></polyline>
               </svg>
